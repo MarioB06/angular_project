f2b73986ae992244318d1011dc021d7afb4f31d1
MarioB06 <mbozic2006@gmail.com>
2024-02-19 13:11:22 +0100

    add bootstrap and a test with a alert

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExampleAComponent } from './example-a/example-a.component';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
   declarations: [ExampleAComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbAlertModule
   ],
   exports: [ExampleAComponent]
 })
